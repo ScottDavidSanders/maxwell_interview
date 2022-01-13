a6bff2377ccf05ded364e6bfdeb4e7b482c5b417
Scott David Sanders <ScottDavidSanders@users.noreply.github.com>
2022-01-12 23:57:59 -0500

    parse, lightly sanitize, and count items

--- price_calculator.js
+++ price_calculator.js
@@ -3,7 +3,22 @@ const readline = require("readline").createInterface({
   output: process.stdout,
 });
 
+const countItems = (itemString) => {
+  const items = itemString.split(",").map((item) => item.trim().toLowerCase());
+  return Object.entries(
+    items.reduce((acc, item) => {
+      if (item in acc) {
+        acc[item]++;
+      } else {
+        acc[item] = 1;
+      }
+      return acc;
+    }, {})
+  ).map(([item, count]) => ({ item, count }));
+};
+
 readline.question("Please enter all the items purchased separated by a comma:\n", (list) => {
-  console.log(list);
+  const blah = countItems(list);
+  console.log(blah);
   readline.close();
 });
